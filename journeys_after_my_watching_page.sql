fc8fbcfb4fbb2fe30da8685ae2dd364cf09517a6
Vicky Banks <victoria.banks052@gmail.com>
2020-07-22 17:28:08 +0100

    Tracked people from my watching page to starting the episode

--- journeys_after_my_watching_page.sql
+++ journeys_after_my_watching_page.sql
@@ -47,7 +47,7 @@ CREATE TABLE watched_journeys_1 AS
            length(replace(LEFT(page_sequence, index), '|', '')) +1                                      AS watch_page_num,   -- Find what page the watching page was
            watch_page_num + 1                                                                           AS ep_page_num,      -- Find what page the ep page was
            REGEXP_INSTR(content_id_sequence, '\\|', 1, ep_page_num) + 1                                 AS pos,-- find the position of this page within the content_id string
-           substring(content_id_sequence, pos, length(content_id_sequence))                             AS content_id_after, -- Get content IDs from the after the search page
+           substring(content_id_sequence, pos-9, length(content_id_sequence))                             AS content_id_after, -- Get content IDs from the after the search page
            split_part(content_id_after, '|', 1)                                                         as content_id
     FROM central_insights_sandbox.dataforce_journey_complete
     WHERE page_sequence LIKE '%watching_page|episode_page%'
@@ -82,7 +82,7 @@ SELECT app_type,
        1                                                                    AS watch_page_num,   -- Find what page the watching page was
        watch_page_num + 1                                                   AS ep_page_num,      -- Find what page the ep page was
        REGEXP_INSTR(content_id_sequence, '\\|', 1, ep_page_num) + 1         AS pos,-- find the position of this page within the content_id string
-       substring(content_id_sequence, pos, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
+       substring(content_id_sequence, pos-9, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the page. -9 as length of content_id
        split_part(content_id_after, '|', 1)                                 as content_id,
        watched_page_num_cumulative + watch_page_num AS watched_page_num_cumulative,               -- need the page number within the whole visit not just this part
        ep_page_num_cumulative + ep_page_num AS ep_page_num_cumulative                             -- need the page number within the whole visit not just this part
@@ -115,7 +115,7 @@ SELECT app_type,
        1                                                                    AS watch_page_num,   -- Find what page the watching page was
        watch_page_num + 1                                                   AS ep_page_num,      -- Find what page the ep page was
        REGEXP_INSTR(content_id_sequence, '\\|', 1, ep_page_num) + 1         AS pos,-- find the position of this page within the content_id string
-       substring(content_id_sequence, pos, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
+       substring(content_id_sequence, pos-9, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
        split_part(content_id_after, '|', 1)                                 as content_id,
        watched_page_num_cumulative + watch_page_num AS watched_page_num_cumulative,               -- need the page number within the whole visit not just this part
        ep_page_num_cumulative + ep_page_num AS ep_page_num_cumulative                             -- need the page number within the whole visit not just this part
@@ -148,7 +148,7 @@ SELECT app_type,
        1                                                                    AS watch_page_num,   -- Find what page the watching page was
        watch_page_num + 1                                                   AS ep_page_num,      -- Find what page the ep page was
        REGEXP_INSTR(content_id_sequence, '\\|', 1, ep_page_num) + 1         AS pos,-- find the position of this page within the content_id string
-       substring(content_id_sequence, pos, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
+       substring(content_id_sequence, pos-9, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
        split_part(content_id_after, '|', 1)                                 as content_id,
        watched_page_num_cumulative + watch_page_num AS watched_page_num_cumulative,               -- need the page number within the whole visit not just this part
        ep_page_num_cumulative + ep_page_num AS ep_page_num_cumulative                             -- need the page number within the whole visit not just this part
@@ -162,7 +162,7 @@ with journeys AS (
     SELECT app_type, dt, visit_id, hashed_id, journey_after AS page_sequence, content_id_after AS content_id_sequence,
            watched_page_num_cumulative,
            ep_page_num_cumulative
-    FROM watched_journeys_3
+    FROM watched_journeys_4
     WHERE num_time_page_pair_occurs > 1
 )
 SELECT app_type,
@@ -181,7 +181,7 @@ SELECT app_type,
        1                                                                    AS watch_page_num,   -- Find what page the watching page was
        watch_page_num + 1                                                   AS ep_page_num,      -- Find what page the ep page was
        REGEXP_INSTR(content_id_sequence, '\\|', 1, ep_page_num) + 1         AS pos,-- find the position of this page within the content_id string
-       substring(content_id_sequence, pos, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
+       substring(content_id_sequence, pos-9, length(content_id_sequence))     AS content_id_after, -- Get content IDs from the after the search page
        split_part(content_id_after, '|', 1)                                 as content_id,
        watched_page_num_cumulative + watch_page_num AS watched_page_num_cumulative,               -- need the page number within the whole visit not just this part
        ep_page_num_cumulative + ep_page_num AS ep_page_num_cumulative                             -- need the page number within the whole visit not just this part
@@ -189,51 +189,59 @@ SELECT app_type,
 FROM journeys;
 
 --- Checks - how many visits in each table
-SELECT count(visit_id) FROM watched_journeys_1;
-SELECT count(visit_id) FROM watched_journeys_2;
-SELECT count(visit_id) FROM watched_journeys_3;
-SELECT count(visit_id) FROM watched_journeys_4;
-SELECT count(visit_id) FROM watched_journeys_5;
+SELECT count(visit_id) FROM watched_journeys_1; --339,833
+SELECT count(visit_id) FROM watched_journeys_2; -- 65,322
+SELECT count(visit_id) FROM watched_journeys_3; -- 19,316
+SELECT count(visit_id) FROM watched_journeys_4; --  7,790
+SELECT count(visit_id) FROM watched_journeys_5; --  3,921
 
 
--- journeys after watching_page|episode_page first ex
-WITH vb_after_first AS (
-    -- Get the first example of watching_page|episode_page in the visit. Find the page number and the id of the content.
-    SELECT app_type,
-           dt,
-           visit_id,
-           hashed_id,
-           page_sequence,
-           CHARINDEX('watching_page|episode_page', page_sequence)                                    AS index,                 --position of 'watching_page' within the journey (position is of the 's')
-           RIGHT(page_sequence, length(page_sequence) - index + 1)                                   AS journey_after,         --get everything from this position onwards
-           length(LEFT(page_sequence, index)) -
-           length(replace(LEFT(page_sequence, index), '|', ''))                                      AS page_num_within_visit, -- what number page was 'search_page' within the visit?
-           REGEXP_INSTR(content_id_sequence, '\\|', 1, page_num_within_visit) + 1                    AS pos,-- find the position of this page within the content_id string
-           substring(content_id_sequence, pos, length(content_id_sequence))                          AS content_id_after       -- Get content IDs from the after the search page
-    FROM central_insights_sandbox.dataforce_journey_complete
-    WHERE page_sequence LIKE '%watching_page|episode_page%'
-      AND app_type != 'mobile-app'
-      AND destination = 'PS_IPLAYER'
-      AND dt = 20200601
+-- Join into one table
+DROP TABLE IF EXISTS watched_journeys_complete;
+CREATE TABLE watched_journeys_complete AS
+    SELECT *, watch_page_num as watched_page_num_cumulative, ep_page_num as ep_page_num_cumulative FROM watched_journeys_1;
+
+INSERT INTO watched_journeys_complete
+SELECT * FROM watched_journeys_2;
+
+INSERT INTO watched_journeys_complete
+SELECT * FROM watched_journeys_3;
+
+INSERT INTO watched_journeys_complete
+SELECT * FROM watched_journeys_4;
+
+INSERT INTO watched_journeys_complete
+SELECT * FROM watched_journeys_5;
+
+
+SELECT * FROM watched_journeys_complete
+ORDER BY visit_id LIMIT 200;
+
+
+-- Join with the journeys start watched table to find out what they watched or not
+WITH watched_journeys AS (
+    -- Get the journeys watching_page|episode_page
+SELECT app_type, dt,visit_id, hashed_id, watched_page_num_cumulative, ep_page_num_cumulative, content_id
+    FROM watched_journeys_complete
 ),
      watch_ep_flags AS (
          SELECT a.dt,
                 a.visit_id,
                 a.hashed_id,
                 a.app_type,
-                a.page_num_within_visit + 2                                     as content_page_num,
+                a.ep_page_num_cumulative                                   as content_page_num,
                 b.page_count,
-                split_part(a.content_id_after, '|', 2)                          AS content_id,
+                a.content_id                         AS content_id_1,
                 b.content_id,
                 cast('watching_page' as varchar ) AS click_placement,
                 CASE WHEN b.start_flag = 'iplxp-ep-started' THEN 1 ELSE 0 END   as start_flag,
                 CASE WHEN b.watched_flag = 'iplxp-ep-watched' THEN 1 ELSE 0 END as watched_flag
-         FROM vb_after_first a
+         FROM watched_journeys a
                   LEFT JOIN central_insights_sandbox.dataforce_journey_start_watch_complete b
                             ON a.dt = b.dt AND a.visit_id = b.visit_id AND
-                               split_part(a.content_id_after, '|', 2) = b.content_id AND
-                               (a.page_num_within_visit + 2) = b.page_count
-     )
+                               a.content_id = b.content_id AND
+                               a.ep_page_num_cumulative  = b.page_count
+)
 SELECT click_placement,
        count(distinct hashed_id) AS num_si_users,
        count(visit_id)           as num_clicks,
