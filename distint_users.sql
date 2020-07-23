f84bef2251bf197f190f78bc0d14d99bd4df7655
Vicky Banks <victoria.banks052@gmail.com>
2020-07-23 11:26:45 +0100

    Found distinct users to all featured parts of the site.

--- distint_users.sql
+++ distint_users.sql
@@ -1,10 +1,29 @@
 -- Get all the user hids from across each peice of work and distinct them
 
-SELECT count(DISTINCT hashed_id) FROM rec_all_journeys_complete; -- 30,691
+SELECT count(DISTINCT hashed_id) FROM rec_all_journeys_complete; --       30,691
 SELECT count(DISTINCT hashed_id) FROM watched_all_journeys_complete; -- 127,750
-SELECT count(DISTINCT bbc_hid3) FROM cta_TLEO_clicks; -- 4,556,852
-SELECT count(DISTINCT bbc_hid3) FROM end_of_playback; -- 7,296,023
+SELECT count(DISTINCT bbc_hid3) FROM cta_TLEO_clicks; --  4,556,852
+SELECT count(DISTINCT bbc_hid3) FROM end_of_playback; --  7,296,023
+SELECT count(DISTINCT bbc_hid3) FROM homepage_modules; -- 6,756,069
 
+SELECT count(DISTINCT bbc_hid3) FROM dist_users;
+
+-- All users in one table
+DROP TABLE IF EXISTS dist_users;
+CREATE TABLE dist_users AS
+    SELECT DISTINCT hashed_id AS bbc_hid3 FROM rec_all_journeys_complete;
+
+INSERT INTO dist_users
+SELECT DISTINCT hashed_id AS bbc_hid3 FROM watched_all_journeys_complete;
+
+INSERT INTO dist_users
+SELECT DISTINCT bbc_hid3 FROM cta_TLEO_clicks;
+
+INSERT INTO dist_users
+SELECT DISTINCT bbc_hid3 FROM end_of_playback;
+
+INSERT INTO dist_users
+SELECT DISTINCT bbc_hid3 FROM homepage_modules;
 
 --- CTA clicks on TLEO
 DROP TABLE IF EXISTS  cta_TLEO_clicks;
@@ -51,3 +70,21 @@ SELECT distinct central_insights_sandbox.udf_dataforce_page_type(click_placement
 FROM central_insights_sandbox.vb_foi_2_final_tleo
 WHERE page_type = 'episode_page'
   AND click_container ILIKE '%next%';
+
+--- From homepage modules
+CREATE TABLE homepage_modules AS
+with both_paths_comb AS (
+    -- homepage -> content and homepage -> TLEO -> content
+    SELECT central_insights_sandbox.udf_dataforce_page_type(click_placement) AS page_type,
+           click_container,
+           bbc_hid3
+    FROM central_insights_sandbox.vb_foi_final_no_tleo
+    WHERE click_placement = 'iplayer.tv.page' --homepage
+)
+SELECT a.page_type,
+       a.click_container,
+       a.bbc_hid3
+FROM both_paths_comb a
+WHERE a.click_container = 'module-recommendations-recommended-for-you'
+   OR a.click_container = 'module-watching-continue-watching'
+;
